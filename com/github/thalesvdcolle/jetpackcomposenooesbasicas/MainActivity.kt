1faebeab6658264d76fad71b7a58e78deeaba45e
thalesvdcolle <thales.vdcolle@gmail.com>
2023-08-16 16:01:57 -0300

    firsting model

--- com/github/thalesvdcolle/jetpackcomposenooesbasicas/MainActivity.kt
+++ com/github/thalesvdcolle/jetpackcomposenooesbasicas/MainActivity.kt
@@ -3,13 +3,16 @@ package com.github.thalesvdcolle.jetpackcomposenooesbasicas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import com.github.thalesvdcolle.jetpackcomposenooesbasicas.ui.theme.JetpackComposeNoçoesBasicasTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +20,101 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeNoçoesBasicasTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                MyApp(modifier = Modifier.fillMaxSize())
                 }
             }
+        }
+    }
+
+@Composable
+private fun MyApp(modifier: Modifier = Modifier){
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+    Greetings()
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+       Row(modifier = Modifier .padding(24.dp)) {
+           Column(modifier = Modifier
+               .weight(1f)
+               .padding(bottom = extraPadding)
+           ) {
+               Text(text = "hello")
+               Text(text = name)
+           }
+           Button(onClick = { expanded.value = !expanded.value}) {
+               Text (if (expanded.value) "Show less" else "Show more")
+           }
+       }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackComposeNoçoesBasicasTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackComposeNoçoesBasicasTheme {
-        Greeting("Android")
+        Greetings()
     }
 }
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    JetpackComposeNoçoesBasicasTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
